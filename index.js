53314e31516b069e8c0b2f1bc87a2ce897a4e9e8
iramnavarrete <iramnavarrete@outlook.com>
2020-05-20 17:02:19 -0600

    Remote server

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ const mongoose = require('mongoose')
 const app = require('./app')
 const PORT = process.env.PORT || 3000
 
-const uat = 'mongodb+srv://alonsowb13:41aMlZBI2Ihji96A@testdatabase-axcki.mongodb.net/test?retryWrites=true&w=majority'
+const uat = 'mongodb+srv://Iram:Iram@cluster0-eozbj.mongodb.net/test?retryWrites=true&w=majority'
 const local = 'mongodb://localhost:27017/jobs-db'
 
-mongoose.connect(local, (err, res)=>{
+
+//mongoose.set('port', PORT)
+mongoose.connect(uat, (err, res)=>{
     if(err) return console.log(`Error connecting to the database ${err}`)
 
     console.log('Connection to the database established')
 
     app.listen(PORT, ()=>{
-        console.log(`API Rest running at http://localhost:${PORT}`)
+        console.log(`API Rest running at server:${PORT}`)
     })
 })
